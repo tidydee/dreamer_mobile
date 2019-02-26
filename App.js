7a65c4256ce2ad95a08160b96f40fb5aa5509e7f
Dawid Spamer <dspamer@bclc.com>
2019-02-25 17:56:10 -0800

    ReactNative App setupwith basic API call to GET All Dreams.

---
+++ App.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button, ScrollView, FlatList } from 'react-native';
+
+export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      data: null,
+      loaded: true,
+      error: null
+    }
+  }
+  baseURL = 'http://127.0.0.1:3000';
+
+  getData = (ev) => {
+    this.setState({loaded: false, error: null});
+    let url = this.baseURL + '/dreams';
+    let h = new Headers();
+    h.append('Authorization', 'Bearer DummyJWT-To-BeReplaced');
+    
+    let req = new Request(url, {
+      headers: h,
+      method: 'GET'
+    });
+
+    fetch(req)
+      .then(response=>response.json())
+      .then(this.showData)
+      .catch(this.error)
+  }
+  showData = (data) => {
+    this.setState({loaded: true, data: data});
+    console.log(data);
+
+  }
+  error = (err) => {
+    this.setState({loaded: true,error: err.message});
+  }
+
+  componentDidMount() {
+    // this.getData();
+  }
+  
+  render() {
+    return (
+      <ScrollView contentContainerStyle={styles.container}>
+        {!this.state.loaded && (
+          <Text>LOADING...</Text>
+        )}
+        <Text style={styles.containerText}>Welcome to Dreamer!</Text>
+        <Button 
+          style={styles.button} 
+          title="GET ALL DREAMS" 
+          onPress={this.getData} />
+          { this.state.error !== null && (
+            <Text style={styles.err}>{ this.state.error }</Text>
+          )}
+          { this.state.data && this.state.data.length > 0 && (
+            this.state.data.map( dream => (
+              <Text key={dream._id} style={styles.dreams}>
+                { dream.title }
+              </Text>
+            ))
+          )}
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerText: {
+    color: '#000',
+    fontSize: 25,
+    fontWeight: 'bold'
+  },
+  dreams: {
+    color: '#000',
+    fontSize: 15,
+  },
+  button: {
+    color: 'blue',
+  },
+  err:{
+    color: 'red',
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
+});
---
+++ README.txt
@@ -0,0 +1,43 @@
+- Basic Components:
+    > 
+    > 
+    > 
+
+- App Layout:
+    > Use FlexBox for Layout
+    > All other styling can be done with pretty much CSS
+
+- App STATE:
+    > State is something you get inside of every Component.
+    > State Manages all data that could change inside that Component.
+    > When I update the sate it will automatically update all the bits of information inside the state.
+    > "this.state" = to write out a number from any varibale set within state.
+        >> "this.state" will set a value, but will not update the values used within the Component.
+    > "this.setState" to set a value for a given variable within state and update all values automatically.
+    
+- App Props:
+    > When you have multiple Components, you often want to pass data between those Components. Props is the way to do this.
+    > ReWatch video enough to sink in and practice a lot!!!
+        >> https://www.youtube.com/watch?v=ZS_LbjG1pgI&index=6&list=PLyuRouwmQCjnuUEYW4mnSp891sd3WL_zr
+    > 
+
+- App Geolocation:
+    > 
+    > 
+    > 
+    > 
+    > 
+
+
+- API Calls:
+    > Use 'jsonplaceholder.typicode.com' for placeholder API calls
+    > 
+    > 
+    > 
+    > 
+
+
+Mobile Design:
+Screens:
+    1. Starts with logo then fade to main Screens
+    2. 
