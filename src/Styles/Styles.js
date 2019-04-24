b3d9d22292a8aa6ea401b1415a1b9e91106697b7
Dawid Spamer <dspamer@bclc.com>
2019-04-23 20:09:40 -0700

    Adding App styles sheet and Card icons.

---
+++ src/Styles/Styles.js
@@ -0,0 +1,53 @@
+import { Dimensions } from 'react-native';
+
+export const dimensions = {
+  FULL_HEIGHT: Dimensions.get('window').height,
+  FULL_WIDTH: Dimensions.get('window').width 
+}
+
+export const colors = {
+  PRIMARY: '#FF3366',
+  SECONDARY: '#1C222E',
+  BACKGROUND: '#142131',
+
+  CARD: '#454D59',
+  CARD_TITLE: '#F6F6F7',
+  CARD_THEME: '#ECEDEE ',
+  CARD_INTRO: '#B4B8BC',
+  CARD_DATE: '#B4B8BC',
+
+  ITEM: '#222C3A',
+  ITEM_ICON: '#B4B8BC',
+  ITEM_PLACEHOLDER: '#F2F2F2',
+  ITEM_HEADING: '#B4B8BC',
+  ITEM_TEXT: '#F2F2F2',
+  ITEM_BOTTOM: '#4F5660',
+
+  ITEM_MAIN_TITLE: '#F2F2F2',
+  ITEM_MAIN_THEME_TEXT: '#DDDEE0',
+  ITEM_MAIN_DATE_TIME: '#8A8C90',
+
+  APP_NAV_ICON: '#FF3366',
+  APP_HEADER_ICON: '#FF3366',
+  APP_HEADER_TITLE: '#E2E2E4',
+  BOTTOM_TAB_BAR: '#1C222E',
+  HEADER_TAB_BAR: '#1C222E',
+
+  SEARCH_SECTION: '#003333',
+  SEARCH_PLACEHOLDER_TEXT: '#454D59',
+  SEARCH_BOTTOM_BORDER: '#454D59',
+  SEARCH_ICON: '#454D59',
+  // SearchTopBorder(gradient): '#1A2A6C' - '#EE386E'
+}
+
+export const fonts = {
+
+}
+
+export const padding = {
+
+}
+
+export const margin = {
+
+} 
